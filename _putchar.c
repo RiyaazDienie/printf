318ff8c401a606ed4a46e59efa1f4bbea2421cae
RiyaazDienie <117740112+RiyaazDienie@users.noreply.github.com>
2023-01-27 20:45:02 +0200

    Update _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - prints character c
+ * @c: character that is printed
+ * Return: 0
+*/
+
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
